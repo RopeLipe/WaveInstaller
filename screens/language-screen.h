1150dd56926e0155928038800457a572f30dc26e
RopeLipe <z1n3xubisoft@gmail.com>
2025-06-08 22:40:53 -0600

    Fixes

--- screens/language-screen.h
+++ screens/language-screen.h
@@ -3,9 +3,11 @@
 
 #include <gtk/gtk.h>
 #include <adwaita.h>
+#include "../wave-installer.h" // Include the main application header
 
 G_BEGIN_DECLS
 
-void language_screen_create(struct _WaveInstallerApplication *app);
+// void language_screen_create(struct _WaveInstallerApplication *app);
+void language_screen_create(WaveInstallerApplication *app);
 
 G_END_DECLS
