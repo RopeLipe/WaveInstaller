5bcf5df064d900297ae6d90243d632cefc4241f6
RopeLipe <z1n3xubisoft@gmail.com>
2025-06-08 22:44:00 -0600

    Fixes

--- screens/timezone-screen.h
+++ screens/timezone-screen.h
@@ -3,9 +3,11 @@
 
 #include <gtk/gtk.h>
 #include <adwaita.h>
+#include "../wave-installer.h" // Include the main application header
 
 G_BEGIN_DECLS
 
-void timezone_screen_create(struct _WaveInstallerApplication *app);
+// void timezone_screen_create(struct _WaveInstallerApplication *app);
+void timezone_screen_create(WaveInstallerApplication *app);
 
 G_END_DECLS
